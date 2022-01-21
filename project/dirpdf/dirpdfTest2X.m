d29f915d6ff29709f9e26c772d2e9dd3d81b9b82
alon <alonspinner@gmail.com>
2022-01-21 22:25:41 +0200

    update project

--- project/dirpdf/dirpdfTest2X.m
+++ project/dirpdf/dirpdfTest2X.m
@@ -1,22 +1,48 @@
 N=1000;
 x1=linspace(0,1,N);
 x2 = 1-x1;
+x=[x1;x2];
 
+fig=figure('color',[1,1,1]);
+ax=axes(fig); grid(ax,'on');
+xlabel('class1'); ylabel('class2'); zlabel('probability');
+hold(ax,'on');
+ax.View = [-39.2000   16.8000];
 
-fig=figure;
-ax=axes(fig);
+beta = [1 1;
+        3 2
+        3 3;
+        4 4;
+        5 5;
+        6 6];
+y = zeros(1,N);
 
-beta = [75,25];
-y = zeros(N,1);
-for ii = 1:N
-        y(ii)=dirpdf([x1(ii),x2(ii)],beta);
+vidWriter = VideoWriter('dirpdf2XExample','Motion JPEG AVI');
+vidWriter.FrameRate = 1;
+open(vidWriter);
+
+for ii=1:size(beta,1)
+    cla(ax);
+    for jj = 1:N
+        y(jj)=dirpdf(x(:,jj)',beta(ii,:));
+    end
+    plot3(ax,x(1,:),x(2,:),y);
+    [ymax,kk] = max(y);
+    xstar = x(:,kk);
+    if ii~=1
+    scatter(ax,xstar(1),xstar(2),'filled');
+    title(ax,['\beta_i',...
+        sprintf('= [%d,%d]\n',beta(ii,:)),...
+        ' \pi_i ',...
+        sprintf('= [%.2f,%.2f]\n',xstar)]);
+    else
+        title(ax,['\beta_i',...
+            sprintf('= [%d,%d]\n',beta(ii,:))]);
+    end
+    plot(ax,[1,0],[0,1],'--k')
+
+    drawnow;
+    writeVideo(vidWriter,getframe(fig));
+    pause(2);
 end
-
-[ymax,ii] = max(y);
-xstar = [x1(ii),x2(ii)];
-plot3(x1,x2,y) 
-hold on;
-xlabel('x1');
-ylabel('x2');
-grid on;
-scatter(xstar(1),xstar(2),'filled');
+close(vidWriter);
--- project/dirpdf/dirpdfTest3X.m
+++ project/dirpdf/dirpdfTest3X.m
@@ -6,34 +6,46 @@ a=0; b=1;
 y = zeros(N,1);
 
 fig=figure;
-ax=axes(fig); grid(ax,'on');
-xlabel('x1');
-ylabel('x2');
-zlabel('no-class');
-ax.View = [55,42];
+ax=axes(fig); grid(ax,'on'); hold(ax,'on');
+xlabel(ax,'class1');
+ylabel(ax,'class2');
+zlabel(ax,'class3');
+ax.View = [58.5000, 31.8000];
 
 beta = [1 1 1;
-        1 2 1;
-        1 3 2;
-        1 4 2;];
+        2 2 1;
+        2 2 2;
+        2 4 2;
+        2 6 2
+        1 10 2];
 
-for ii=1:size(beta,1)   
+vidWriter = VideoWriter('dirpdf3XExample','Motion JPEG AVI');
+vidWriter.FrameRate = 1;
+open(vidWriter);
+for ii=1:size(beta,1) 
+    cla(ax);
     for jj = 1:N
-        y(jj)=dirpdf(x(:,ii)',beta(ii,:));
+        y(jj)=dirpdf(x(:,jj)',beta(ii,:));
     end
-    scatter3(x(1,:),x(2,:),x(3,:),5,y);
+    scatter3(ax,x(1,:),x(2,:),x(3,:),5,y);
+    
+    [ymax,kk] = max(y);
+    xstar = x(:,kk);
+    if ii~=1
+    scatter(ax,xstar(1),xstar(2),'filled');
+    title(ax,['\beta_i',...
+        sprintf('= [%d,%d,%d]\n',beta(ii,:)),...
+        ' \pi_i ',...
+        sprintf('= [%.2f,%.2f,%.2f]\n',xstar)]);
+    else
+        title(ax,['\beta_i',...
+            sprintf('= [%d,%d,%d]\n',beta(ii,:))]);
+    end
+    
     drawnow;
-    pause(1);
+    writeVideo(vidWriter,getframe(fig));
+    pause(2);
 end
-
-
-
-
-
-
-
-%%
-
-
+close(vidWriter);
 
 
